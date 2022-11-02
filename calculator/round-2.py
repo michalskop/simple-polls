59cd89d4076ae182f07329caea6cffd885291e20
Michal Skop <michal.skop@kohovolit.eu>
2022-11-02 01:01:21 +0100

    more than 2 candidates supported

--- calculator/round-2.py
+++ calculator/round-2.py
@@ -1,5 +1,7 @@
 """Calculator for second rounds."""
 
+# Note: more than 2 candidates is supported: 2 main candidates + "others"
+
 import datetime
 import datetime
 import gspread
@@ -38,14 +40,14 @@ def aging_coeff(day1, day2):
   return pow(diff, 1.15) / diff
 
 # normal error
-def normal_error(p, n, volatility, coef = 1):
-  p['sdx'] = (n * p['p'] * (1 - p['p'])).apply(math.sqrt) / n * coef * volatility
+def normal_error(p, i, n, volatility, coef = 1):
+  p['sdx'] = (n * p['p' + str(i)] * (1 - p['p' + str(i)])).apply(abs).apply(math.sqrt) / n * coef * volatility
   p['normal_error'] = scipy.stats.norm.rvs(loc=0, scale=p['sdx'])
   return p
 
 # uniform_error as function of normal error
-def uniform_error(p, n, volatility, coef = 1):
-  p['sdx'] = (n * p['p'] * (1 - p['p'])).apply(math.sqrt) / n * coef * volatility
+def uniform_error(p, i, n, volatility, coef = 1):
+  p['sdx'] = (n * p['p' + str(i)] * (1 - p['p' + str(i)])).apply(abs).apply(math.sqrt) / n * coef * volatility
   p['uniform_error'] = scipy.stats.uniform.rvs(loc=(-1 * p['sdx'] * math.sqrt(3)), scale=(2 * p['sdx'] * math.sqrt(3)))
   return p
 
@@ -53,25 +55,32 @@ def uniform_error(p, n, volatility, coef = 1):
 dfpreference = dfpreference[dfpreference['name'] != '']
 
 # p
-dfpreference['p'] = dfpreference['gain1'] / 100
+dfpreference['p1'] = dfpreference['gain1'] / 100
+dfpreference['p3'] = 1 - dfpreference['p1'] - dfpreference['gain2'] / 100
 
 # simulations
 # simulations = pd.DataFrame(columns=fpreference['name'].to_list())
-simulations_aging = pd.DataFrame(columns=dfpreference['name'].to_list())
+simulations_aging = {}
+for i in range(1, 4):
+  simulations_aging[i] =pd.DataFrame(columns=dfpreference['name'].to_list())
 aging = aging_coeff(today, election_day)
 
-for i in range(0, sample):
-  p = normal_error(dfpreference, sample_n, volatility, 1)
-  p = uniform_error(p, sample_n, volatility, 1.5 * 0.9)
-  # p['estimate'] = p['normal_error'] + p['uniform_error'] + p['p']
-  p['estimate_aging'] = aging * (p['normal_error'] + p['uniform_error']) + p['p']
-  # simx = dict(zip(dfpreference['party'].to_list(), p['estimate']))
-  simxa = dict(zip(dfpreference['name'].to_list(), p['estimate_aging']))
-  # simulations = simulations.append(simx, ignore_index=True)
-  simulations_aging = simulations_aging.append(simxa, ignore_index=True)
+for j in range(0, sample):
+  for i in {1, 3}:
+    p = normal_error(dfpreference, i, sample_n, volatility, 1)
+    p = uniform_error(p, i, sample_n, volatility, 1.5 * 0.9)
+    # p['estimate'] = p['normal_error'] + p['uniform_error'] + p['p']
+    p['estimate_aging'] = aging * (p['normal_error'] + p['uniform_error']) + p['p' + str(i)]
+    # simx = dict(zip(dfpreference['party'].to_list(), p['estimate']))
+    simxa = dict(zip(dfpreference['name'].to_list(), p['estimate_aging']))
+    # simulations = simulations.append(simx, ignore_index=True)
+    simulations_aging[i] = pd.concat([simulations_aging[i], pd.DataFrame([simxa])], ignore_index=True)
+    # simulations_aging = simulations_aging.append(simxa, ignore_index=True)
+
+simulations_aging[2] = 1 - simulations_aging[1] - simulations_aging[3]
 
 # rank
-winning = pd.DataFrame((simulations_aging >= 0.5).sum(axis=0) / sample).rename(columns={0: 'p1'})
+winning = pd.DataFrame((simulations_aging[1] >= simulations_aging[2]).sum(axis=0) / sample).rename(columns={0: 'p1'})
 winning['p2'] = 1 - winning['p1']
 
 # write to GSheet
@@ -80,17 +89,21 @@ wsw.update('A1', [winning.reset_index().columns.values.tolist()] + winning.reset
 wsw.update_acell('A1', 'Pr[duel winned]')
 
 # less than
-interval_statistics_aging = pd.DataFrame(columns=dfpreference['name'].to_list())
-interval = pd.DataFrame(columns=['Pr[duel zisk > x %]'])
+interval_statistics_aging = {}
+interval = {}
+for j in range(1, 4):
+  interval_statistics_aging[j] = pd.DataFrame(columns=dfpreference['name'].to_list())
+  interval[j] = pd.DataFrame(columns=['Pr[duel zisk > x %]'])
 # for i in np.concatenate((np.arange(0, interval_max + 0.5, 0.5), np.array([26.33, 22.79, 17.11, 9.13, 8.51]))):
-for i in np.concatenate((np.arange(interval_min, interval_max + step, step), np.array([]))):
-    interval = interval.append({'Pr[duel zisk > x %]': i}, ignore_index=True)
-    interval_statistics_aging = interval_statistics_aging.append((simulations_aging > (i / 100)).sum() / sample, ignore_index=True)
+  for i in np.concatenate((np.arange(interval_min, interval_max + step, step), np.array([]))):
+    interval[j] = interval[j].append([{'Pr[duel zisk > x %]': i}], ignore_index=True)
+    interval_statistics_aging[j] = interval_statistics_aging[j].append((simulations_aging[j] > (i / 100)).sum() / sample, ignore_index=True)
 
 # write to GSheet
-wsw = sh.worksheet('pravděpodobnosti')
-interval[interval_statistics_aging.columns] = interval_statistics_aging
-wsw.update('A1', [interval.columns.values.tolist()] + interval.values.tolist())
+for j in range(1, 3):
+  wsw = sh.worksheet('pravděpodobnosti' + str(j))
+  interval[j][interval_statistics_aging[j].columns] = interval_statistics_aging[j]
+  wsw.update('A1', [interval[j].columns.values.tolist()] + interval[j].values.tolist())
 
 # write datetime
 wsw = sh.worksheet('parametry')
